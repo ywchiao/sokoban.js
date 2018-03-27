e7ab3b2261708952d8015132af6a3e40439bedf8
Yiwei Chiao <ywchiao@gmail.com>
2018-03-27 13:52:22 +0800

    combin code with documents.

--- httpd/index.js
+++ httpd/index.js
@@ -3,7 +3,7 @@
  *  @brief      The entry function of the httpd.
  *  @author     Yiwei Chiao (ywchiao@gmail.com)
  *  @date       10/03/2017 created.
- *  @date       10/03/2017 last modified.
+ *  @date       11/17/2017 last modified.
  *  @version    0.1.0
  *  @copyright  MIT, (C) 2017 Yiwei Chiao
  *  @details
@@ -14,18 +14,63 @@
 
 let http = require('http');
 
+const routingTable = require('./config.json');
+
+/**
+  * 利用 http.ServerResponse 物件回傳檔案內容
+  *
+  * @name serve
+  * @function
+  * @param response - http.ServerResponse 物件
+  * @param fname - 要回傳的檔案名
+  * @param datatype - 回傳檔案內容的 Mine-Type
+  * @returns {undefined}
+  */
+let serve = (response, fname, datatype) => {
+  let fs = require('fs');
+
+  fs.readFile(fname, (err, data) => {
+    if (err) {
+      console.log('檔案讀取錯誤：' + fname);
+    }
+    else {
+      response.writeHead(200, {
+        'Content-Type': datatype
+      });
+
+      response.write(data);
+      response.end();
+    }
+  });
+};
+
 http.createServer((request, response) => {
-  // 傳送 HTTP header
-  // HTTP Status: 200 : OK
-  // Content Type: text/plain
-  response.writeHead(200, {
-    'Content-Type': 'text/plain'
+  let fs = require('fs');
+
+  let postData = '';
+
+  // 利用 'data' event 消耗掉 data chunk;
+  // 'end' event 才會被 fired
+  request.on('data', (chunk) => {
+    postData += chunk;
+
+    console.log(
+      '接收的 POST data 片段: [' + chunk + '].'
+    );
   });
 
-  // 傳送回應內容。
-  response.end('Hello World!\n');
+  request.on('end', () => {
+    if (request.url in routingTable) {
+      let obj = routingTable[request.url];
 
-  console.log('request.headers: \n', request.headers)
+      serve(response, obj.url, obj.type);
+    }
+    else {
+      console.log('未定義的存取: ' + request.url);
+
+      response.end();
+    }
+  });
 }).listen(8088);
 
 // log message to Console
